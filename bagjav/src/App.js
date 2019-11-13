28af3f1e998b659e4a09e9200f51f0aa1a9b764c
gabesharpton <gabriel.sharpton@gmail.com>
2019-11-13 12:14:03 -0600

    created intro page

--- bagjav/src/App.js
+++ bagjav/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
-
+import Intro from './Intro'
 
 function App() {
   return (
     <div className="App">
-     <h1> Welcome to Bagjav!</h1>
-     
+     <Intro />
     </div>
   );
 }
