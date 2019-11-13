357249e5792d1dbaa520237df55c42427d7a6c1c
gabesharpton <gabriel.sharpton@gmail.com>
2019-11-13 14:07:15 -0600

    working on game logic

--- bagjav/src/Computer.js
+++ bagjav/src/Computer.js
@@ -10,3 +10,7 @@ function App() {
 }
 
 export default App;
+//Computer should automatically match card with the one being played in set.
+//Computer should have same restrictions as players for what can be played.
+//Computer should wait a second then play card(s)
+//Computer can only attack with one card
--- bagjav/src/Player.js
+++ bagjav/src/Player.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+function Player() {
   return (
     <div className="App">
      <h1> Welcome to Bagjav!</h1>
@@ -9,4 +9,9 @@ function App() {
   );
 }
 
-export default App;
+export default Player;
+
+//Player should be able to choose card from their hand and select player they want to attack
+//PLayer cannot play card unless it is their turn 
+//Player cannot play ace unless they do not have the suit and have not been attacked that set
+
