bfce356f996841338c62e85e8d9ef9d35d36b564
gabesharpton <gabriel.sharpton@gmail.com>
2019-11-13 17:33:41 -0600

    cards appear in console and are shuffled

--- bagjav/src/App.js
+++ bagjav/src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { Route, Link } from "react-router-dom"
 import './App.css';
 import Intro from './Intro'
+import Cards from './Card'
 
-function App() {
+
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state= deck1
+  }
+  render(){
   return (
     <div className="App">
-     <Intro />
+      <nav>
+        <Link to="">Intro</Link>
+      </nav>
+      <Route path="" render={Intro}/>
+     
+   
     </div>
   );
 }
-
+}
 export default App;
+
+const deck1 = new Cards();
+        console.log(deck1);
+        // deck1.reset();
+        // console.log(deck1.deck);
--- bagjav/src/Card.js
+++ bagjav/src/Card.js
@@ -2,37 +2,81 @@
 export default class Cards {
     constructor(){
         this.deck = [];
-        this.reset();
+        this.makeCards();
         this.shuffle();
+        this.player = {
+            player1: [],
+            player2: [],
+            player3: [],
+            player4: []
+        }
+        this.deal();
     }
-    reset(){
+    makeCards(){
         this.deck = [];
         const suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds']
         const values = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
 
+        
         for (let suit in suits){
             for (let value in values) {
                 this.deck.push(`${values[value]} of ${suits[suit]}`);
             }
         }
+        return this.deck
     }
 
         shuffle() {
             const {deck} = this;
             let j = deck.length, i;
+            
 
             while(j) {
                 i = Math.floor(Math.random() * j--);
                 [deck[j], deck[i]] = [deck[i], deck[j]]
+                // console.log([deck[i], deck[j]])
             }
             return this;
         }
         deal(){
-            return this.deck.pop();
+            // for(let players in this.player){
+                for(let c = 0; c <= 13; c++){
+                    this.deck.shift(this.player.player1)
+                    // this.player.push(this.player.player1)
+                }
+            return ;
         }
 
+    // }
+  
+
+    // function deals(){
+
+                
+
+
+
+
+
+
+        // let cardsInHand = 13;
+        // let player = ["'player1': cardsInHand, 'player2': cardsInHand, 'player3', 'player4'"];
+
+        // for (let player = 0; player < 4; player++){
+        //   console.log(player)
+        //   for(let cardsInHand = 0; cardsInHand < deck1.length; cardsInHand++){
+        //     console.log(deck1[0])
+        //     }
+        // }
+
+        // for(cardsInHand in player){
+        //     deck1.push(cardsInHand[player])
+        //     console.log(player[0])
+        // }
+                //}
+
+                
+                
+        // console.log(deck1)
+        // deals()
 }
-const deck1 = new Cards();
-console.log(deck1.deck);
-deck1.reset();
-console.log(deck1.deck);
