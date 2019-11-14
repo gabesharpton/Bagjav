fa400cada2ea44716bc910cab7fbe31440c7fd72
gabesharpton <gabriel.sharpton@gmail.com>
2019-11-14 17:34:44 -0600

    dealing works with setState

--- bagjav/src/Card.js
+++ bagjav/src/Card.js
@@ -13,14 +13,13 @@ export default class Cards extends Component{
             playCard: []
             
         }
-        this.makeCards();
+        //this.startGame();
+        this.makeCards(); 
         this.shuffle();
-        this.deal();
-        this.startGame();
         // this.checkCard();
     }
+
     makeCards = () => {
-        this.deck = [];
         const suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds']
         const values = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
 
@@ -30,6 +29,7 @@ export default class Cards extends Component{
                 this.state.deck.push(<span className='cards' key={value+suit}>{values[value]}</span>);
             }
         }
+        // this.shuffle();
         return this.deck
     }
 
@@ -43,50 +43,61 @@ export default class Cards extends Component{
                 [deck[j], deck[i]] = [deck[i], deck[j]]
                 // console.log([deck[i], deck[j]])
             }
+            //this.deal();
             return this;
     }
-        deal = () => {
-
-                // let deals = this.state.deck.splice(0, 13)
-                this.setState(state => {
-                    let player1 = this.state.deck.splice(0, 13)
-                    return player1;
-                })
-            // this.state.player1 = this.state.deck.splice(0, 13)
-            this.state.player2 = this.state.deck.splice(0, 13)
-            this.state.player3 = this.state.deck.splice(0, 13)
-            this.state.player4 = this.state.deck.splice(0, 13)
-
-            return this.state;
-        
+    deal = () => {
+            this.setState({ player1: this.state.deck.filter((cards, index) => {
+                    return index < 13 ? cards : null
+                }) 
+            }) 
+            this.setState({ player2: this.state.deck.filter((cards, index) => {
+                    return index >= 13 && index < 26 ? cards : null
+                }) 
+            }) 
+            this.setState({ player3: this.state.deck.filter((cards, index) => {
+                    return index >= 26 && index < 39 ? cards : null
+                }) 
+            }) 
+            this.setState({ player4: this.state.deck.filter((cards, index) => {
+                    return index >= 39 && index <= 52 ? cards : null
+                }) 
+            }) 
     }
 
     startGame = () => {
         // let choice = prompt("Would you like to start the game?")
         // if(choice === 'yes'){
-        this.choseCard()
+        // this.choseCard()
         // console.log(this.playCard)
         // this.state.playCard = this.state.player2.filter(() => this.state.playCard)
         // console.log(this.state.children)
         return;
     }
     choseCard(){
-        this.playCard = this.state
-        // this.state.playCard = this.state.player1.splice(0, 1)
-        // console.log(this.state.playCard["0"].props.children[0])
-        // this.state.playCard = this.state.player2.filter(this.checkCard(this.cardValue, this.state.playCard))
-        // console.log(this.state.playCard["0"].key[this.value])
-
-        //I need to sort player2 array to find props.children[0]
+        this.setState({playCard: this.state.player1.splice(0,1)})
+        //I need to filter player2 array to find props.children[0]
         //if it has that then push to this.state.playCard
         //Player 3 sorts through array to find card etc....
     }
-
-    checkCard (cardValue) {
-        return this.cardValue = this.state.playCard["0"].props.children[0]
-
+    player2turn() {
+        this.setState({playCard: this.state.player2.filter((card) => {
+            return card === this.state.playCard[0].props.children[0]
+        })})
     }
+    
+    
+    // checkCard (cardValue) {
+        //     return this.cardValue = this.state.playCard["0"].props.children[0]
+        
+        // }
         render(){
+            console.log( this.state.player1 )
+            console.log( this.state.player2 )
+            console.log( this.state.player3 )
+            console.log( this.state.player4 )
+            console.log(this.state.playCard)
+            // console.log(this.state.playCard["0"].props.children[0])
             return(
                 <div>
                     {/* <p>{this.state.deck}</p> */}
@@ -94,7 +105,11 @@ export default class Cards extends Component{
                 <p id='player2'>{this.state.player2}</p>
                 <p id='player3'>{this.state.player3}</p>
                 <p id='player4'>{this.state.player4}</p>
-                <p>{this.state.playCard}</p>
+                <button onClick={() => this.deal()} >Deal</button>
+                <button onClick={() => this.choseCard()} >chose card</button>
+                <button onClick={() => this.player2turn()} >PLayer 2</button>
+                
+                <p>{this.state.playCard} this one</p>
                 <p>{this.state.playCard.length}</p>
                 </div>
             )
