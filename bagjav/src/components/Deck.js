a87c0970c3438412f12bdb550b60131df56ac83b
gabesharpton <gabriel.sharpton@gmail.com>
2019-11-16 16:42:12 -0600

    finished filter method for other players playing playcard

---
+++ bagjav/src/components/Deck.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import './App.css';
+//import Card from './Card'
+
+function Deck(props) {
+  // let deck = props.deck.map((cards) => {
+  //   return <Card key={cards.value}/>
+  // })
+  return (
+    <div className="App">
+     {/* <h1> Welcome to Bagjav!</h1> */}
+     
+     {/* <p>{deck}</p> */}
+    </div>
+  );
+}
+
+export default Deck;
