b420b272d5ec29c82885e05e83a0644e977614b2
gabesharpton <gabriel.sharpton@gmail.com>
2019-11-26 16:49:10 -0600

    back to deployed state

---
+++ src/Computer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1> Welcome to Bagjav!</h1>
+    </div>
+  );
+}
+
+export default App;
+//Computer should automatically match card with the one being played in set.
+//Computer should have same restrictions as players for what can be played.
+//Computer should wait a second then play card(s)
+//Computer can only attack with one card
