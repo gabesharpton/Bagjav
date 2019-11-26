dc19d8b3e47dd21fd070ac748b6146d684eba7ad
gabesharpton <gabriel.sharpton@gmail.com>
2019-11-26 17:03:18 -0600

    Updates

---
+++ precache-manifest.d263fe00288f96d1c0d993efda741329.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49e451ba1c94344c0ac4ab104d190de5",
+    "url": "/Bagjav/index.html"
+  },
+  {
+    "revision": "f1a0a18f3ea05bbd1788",
+    "url": "/Bagjav/static/css/main.f46a4a37.chunk.css"
+  },
+  {
+    "revision": "c6ab16520f538375605b",
+    "url": "/Bagjav/static/js/2.1ece3417.chunk.js"
+  },
+  {
+    "revision": "f1a0a18f3ea05bbd1788",
+    "url": "/Bagjav/static/js/main.8f4acc90.chunk.js"
+  },
+  {
+    "revision": "b94b1923512e59b076c5",
+    "url": "/Bagjav/static/js/runtime-main.cf12a997.js"
+  }
+]);
